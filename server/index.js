a7853698614361bab15e6f7c5734288993f13e93
erichen886 <erichen886@gmail.com>
2020-07-23 19:14:16 -0700

    add listening port

--- server/index.js
+++ server/index.js
@@ -15,6 +15,6 @@ app.use(cors());
 // app.use('/reviews', createProxyMiddleware({ target: 'http://18.224.95.187/', changeOrigin: true }));
 // app.use('/carousel', createProxyMiddleware({ target: 'http://54.215.84.53/', changeOrigin: true }));
 // app.use('/api', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
-app.use('/api', createProxyMiddleware({ target: 'http://ec2-52-53-224-53.us-west-1.compute.amazonaws.com', changeOrigin: true}));
+app.use('/photogallery', createProxyMiddleware({ target: 'http://ec2-52-53-224-53.us-west-1.compute.amazonaws.com', changeOrigin: true}));
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}/`));
+app.listen(port, () => console.log(`Listening at http://ec2-18-144-100-18.us-west-1.compute.amazonaws.com:${port}/`));
